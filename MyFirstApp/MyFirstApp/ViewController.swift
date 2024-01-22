5526df1e7d19e7a691801eae30a6662db815d1a4
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-01-22 15:40:24 +0900

    텍스트 및 버튼 설정

--- MyFirstApp/MyFirstApp/ViewController.swift
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        mainLabel.backgroundColor = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)
+        mainLabel.text = "안녕하세요"
+        
+        mainLabel.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        
+        mainLabel.textAlignment = NSTextAlignment.center
+    }
+    
 
 }
 
