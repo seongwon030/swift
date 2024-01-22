0a3ca3293e6cb68ee6cfba75d0fc8ab49c5d6498
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-01-22 21:42:55 +0900

    Dice App

---
+++ DiceGame/DiceGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceGame
+//
+//  Created by 서성원 on 2024/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
