9d8f78e03b2a7fd0d4e7223e2c7a6a694cc1b55c
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-01-28 00:41:48 +0900

    Rps 완료

---
+++ RPSGame/RPSGame/Rps.swift
@@ -0,0 +1,16 @@
+//
+//  RPS.swift
+//  RPSGame
+//
+//  Created by 서성원 on 2024/01/28.
+//
+
+import Foundation
+
+// 원시값 (raw value)
+
+enum Rps: Int {
+    case rock
+    case paper
+    case scissors
+}
--- RPSGame/RPSGame/ViewController.swift
+++ RPSGame/RPSGame/ViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  ViewController.swift
 //  RPSGame
@@ -18,23 +19,103 @@ class ViewController: UIViewController {
     @IBOutlet weak var comChoiceLabel: UILabel!
     @IBOutlet weak var myChoiceLabel: UILabel!
     
+    var myChoice: Rps = Rps.rock
     
+    var comChoice: Rps = Rps(rawValue: Int.random(in: 0...2))!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        comImageView.image = #imageLiteral(resourceName: "ready")
+        myImageView.image = UIImage(named: "ready.png")
+        
+        comChoiceLabel.text = "준비"
+        myChoiceLabel.text = "준비"
+    
+        
     }
     
     
     @IBAction func rpsButtonTapped(_ sender: UIButton) {
+        // 가위 바위 보를 선택해  그 정보를 저장
+        let title = sender.currentTitle!
+        print(title)
+        
+        switch title {
+        case "가위":
+            myChoice = Rps.scissors
+        case "바위":
+            myChoice = Rps.rock
+        case "보":
+            myChoice = Rps.paper
+        default:
+            break
+        }
     }
     
     
     @IBAction func selectButtonTapped(_ sender: UIButton) {
+        
+        // 1) 컴퓨터가 랜덤 선택한 것을 이미지뷰에 표시
+        
+        // 2) 컴퓨터가 랜덤 선택한 것을 레이블에 푯
+        switch comChoice {
+        case Rps.rock:
+            comImageView.image = #imageLiteral(resourceName: "rock")
+            comChoiceLabel.text = "바위"
+        case Rps.paper:
+            comImageView.image = #imageLiteral(resourceName: "paper")
+            comChoiceLabel.text = "보"
+        case Rps.scissors:
+            comImageView.image = #imageLiteral(resourceName: "scissors")
+            comChoiceLabel.text = "가위"
+        }
+        // 3) 내가 선택한 것을 이미지뷰에 표시
+        // 4) 내가 선택한 것을 레이블에 표시
+        switch myChoice {
+        case Rps.rock:
+            myImageView.image = #imageLiteral(resourceName: "rock")
+            myChoiceLabel.text = "바위"
+        case Rps.paper:
+            myImageView.image = #imageLiteral(resourceName: "paper")
+            myChoiceLabel.text = "보"
+        case Rps.scissors:
+            myImageView.image = #imageLiteral(resourceName: "scissors")
+            myChoiceLabel.text = "가위"
+        }
+        // 5) 컴퓨터가 선택한 것과 내가 선택한 것을 비교해 결과 표시
+        
+        if comChoice == myChoice {
+            mainLabel.text = "비겼다"
+        } else if comChoice == .rock && myChoice == .paper {
+            mainLabel.text = "이겼다"
+        } else if comChoice == .paper && myChoice == .scissors {
+            mainLabel.text = "이겼다"
+        } else if comChoice == .scissors && myChoice == .rock {
+            mainLabel.text = "이겼다"
+        } else {
+            mainLabel.text = "졌다"
+        }
     }
     
     @IBAction func resetButtonTapped(_ sender: UIButton) {
+        
+        // 1) 컴퓨터가 다시 준비 이미지뷰에 표시
+        // 2) 컴퓨터가 다시 준비 레이블에 표시
+        comImageView.image = #imageLiteral(resourceName: "ready")
+        comChoiceLabel.text = "준비"
+        // 3) 내 선택 이미지뷰에도 준비 이미지를 표시
+        // 4) 내 선택 레이블에도 준비 문자열 표시
+        myImageView.image = #imageLiteral(resourceName: "ready")
+        myChoiceLabel.text = "준비"
+        // 5) 메이 레이블에 선택하세요 라고 표시
+        mainLabel.text = "선택하세요"
+        
+        // 6) 컴퓨터가 다시 랜덤 가위바위보를 선택하고 저장
+        comChoice = Rps(rawValue: Int.random(in: 0...2))!
     }
+    
+    
     
 }
 
