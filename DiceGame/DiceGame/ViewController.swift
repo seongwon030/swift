48ce12428c7e490ba8dfc50aebff42f7f6fedaae
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-01-27 16:08:08 +0900

    dicegame 완료

--- DiceGame/DiceGame/ViewController.swift
+++ DiceGame/DiceGame/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var firstImageView: UIImageView!
+    @IBOutlet weak var secondImageView: UIImageView!
+    
+    var diceArray: [UIImage] = [#imageLiteral(resourceName: "black1"), #imageLiteral(resourceName: "black2"), #imageLiteral(resourceName: "black3"), #imageLiteral(resourceName: "black4"), #imageLiteral(resourceName: "black5"), #imageLiteral(resourceName: "black6")]
+                                
+                                
+                                
+    // 앱에 들어오면 가장 먼저 실행되는 함수
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        firstImageView.image = diceArray[1]
+        secondImageView.image = diceArray[2]
     }
 
 
+    @IBAction func rollButtontapped(_ sender: UIButton) {
+        
+        // 첫 번째 이미지뷰의 이미지를 랜덤으로 변경
+        firstImageView.image = diceArray.randomElement()
+
+        // 두 번째 이미지뷰의 이미지를 랜덤으로 변경
+        secondImageView.image = diceArray.randomElement()
+    }
+    
 }
 
