be24abd9f97f89200ebda2afc7ce4cf192abfc28
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-01-27 18:00:35 +0900

    화면 코드 간 연결

--- RPSGame/RPSGame/ViewController.swift
+++ RPSGame/RPSGame/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    @IBOutlet weak var comImageView: UIImageView!
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBOutlet weak var comChoiceLabel: UILabel!
+    @IBOutlet weak var myChoiceLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
+    
+    
+    @IBAction func rpsButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func selectButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
