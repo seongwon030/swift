b575246c4b4e29fc8e5d84071493f614adcc1121
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2024-02-08 16:01:36 +0900

    랜덤빙고

---
+++ UpDownGame/UpDownGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UpDownGame
+//
+//  Created by 서성원 on 2024/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
